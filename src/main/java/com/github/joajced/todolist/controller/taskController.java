8b967e5c3799af15accbac805612e745514596a6
Joaquin Jimenez Cedron <joaquin.jzcd@gmail.com>
2024-09-30 18:15:41 +0200

    Add PUT and PATCH mappings

--- src/main/java/com/github/joajced/todolist/controller/taskController.java
+++ src/main/java/com/github/joajced/todolist/controller/taskController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 @RestController
@@ -31,12 +32,48 @@ public class taskController {
 
         if (optionalTask.isPresent())
             return optionalTask.get();
-        throw new RuntimeException("Task with id " + id + " couldn't be found");
+        throw new RuntimeException("Task with id " + id + " does not exist.");
     }
 
     @PostMapping("/api/tasks")
     public Task createTask(@RequestBody Task task) {
         return taskRepository.save(task);
+    }
+
+    @PutMapping("/api/tasks/{id}")
+    public Task updateTask(@PathVariable Long id, @RequestBody Task newTask) {
+
+        Optional<Task> needsUpdate = taskRepository.findById(id);
+
+        if (needsUpdate.isPresent()) {
+            needsUpdate.get().setContent(newTask.getContent());
+            needsUpdate.get().setDone(newTask.isDone());
+
+            return taskRepository.save(needsUpdate.get());
+        }
+        throw new RuntimeException("Task with id " + id + " does not exist.");
+    }
+
+    @PatchMapping("/api/tasks/{id}")
+    public Task patchTask(@PathVariable Long id, @RequestBody Map<String, Object> fields) {
+
+        Optional<Task> needsPatch = taskRepository.findById(id);
+
+        if (needsPatch.isPresent()) {
+            fields.forEach((key, value) -> {
+                switch (key) {
+                    case "content":
+                        needsPatch.get().setContent((String) value);
+                        break;
+                    case "done":
+                        needsPatch.get().setDone((Boolean) value);
+                        break;
+                }
+            });
+
+            return taskRepository.save(needsPatch.get());
+        }
+        throw new RuntimeException("Task with id " + id + " does not exist.");
     }
 
     @DeleteMapping("/api/tasks/{id}")
--- src/main/java/com/github/joajced/todolist/model/Task.java
+++ src/main/java/com/github/joajced/todolist/model/Task.java
@@ -22,6 +22,6 @@ public class Task {
     private String content;
 
     @Column
-    private boolean done;
+    private boolean isDone;
 
 }
