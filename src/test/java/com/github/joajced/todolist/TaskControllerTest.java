deb303578ca381fa59871e50cde3353f5bb02ab9
Joaquin Jimenez Cedron <joaquin.jzcd@gmail.com>
2025-01-05 19:42:13 +0100

    Write basic test for TaskController

---
+++ src/test/java/com/github/joajced/todolist/TaskControllerTest.java
@@ -0,0 +1,19 @@
+package com.github.joajced.todolist;
+
+import com.github.joajced.todolist.controller.TaskController;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskControllerTest {
+
+    TaskController taskController;
+
+    @Test
+    public void testGetTasks() {
+
+        assertTrue(true);
+    }
+}
